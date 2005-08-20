2a443a759c1d893eea192813b9d20e2292b7f322
maruo_syunsuke <maruo_syunsuke@64d2bceb-c1f7-0310-a597-f30c3359d46a>
2005-08-20 07:32:20 +0000

    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/maya@442 64d2bceb-c1f7-0310-a597-f30c3359d46a

---
+++ src-standard/org/seasar/maya/standard/engine/processor/jstl/fmt/FormatUtil.java
@@ -0,0 +1,42 @@
+package org.seasar.maya.standard.engine.processor.jstl.fmt;
+
+import java.util.Locale;
+
+import org.seasar.maya.impl.util.StringUtil;
+
+public class FormatUtil {
+    /**
+     * Locale文字列をパースし、Localeオブジェクトを返す。
+     * 
+     * @param localeString
+     *            Locale文字列
+     * @return LocaleObject
+     * @throws IllegalArgumentException
+     *             形式不正の場合
+     */
+    public static Locale parseLocale(String localeString) {
+        int index = -1;
+        String language;
+        String country;
+
+        if (( index = localeString.indexOf('-')) >= 0 ||
+            ( index = localeString.indexOf('_')) >= 0 ){
+            language = localeString.substring(0, index);
+            country  = localeString.substring(index+1);
+        } else {
+            language = localeString;
+            country = null;
+        }
+
+        if (StringUtil.isEmpty(language)
+                || (country != null && country.length() == 0)) {
+            throw new IllegalArgumentException("invalid Locale: " + localeString);
+        }
+
+        if (country == null) {
+            country = "";
+        }
+
+        return new Locale(language, country);
+    }
+}

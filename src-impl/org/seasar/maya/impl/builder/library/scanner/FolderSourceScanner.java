350b6bbb9045294d5a3e3b6029391e50ba5112c4
SUGA Koji <ko.suga@gmail.com>
2005-09-06 04:52:34 +0000

    WEB-INF以下を再帰的に見るよう修正
    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/maya@726 64d2bceb-c1f7-0310-a597-f30c3359d46a

--- src-impl/org/seasar/maya/impl/builder/library/scanner/FolderSourceScanner.java
+++ src-impl/org/seasar/maya/impl/builder/library/scanner/FolderSourceScanner.java
@@ -16,7 +16,9 @@
 package org.seasar.maya.impl.builder.library.scanner;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.seasar.maya.builder.library.scanner.SourceScanner;
@@ -32,42 +34,47 @@ import org.seasar.maya.impl.util.collection.NullIterator;
 public class FolderSourceScanner implements SourceScanner {
 
     private ApplicationSourceDescriptor _source;
+
     private String _folder;
-    
+
+    private boolean _recursive = false;
+
     public void setParameter(String name, String value) {
-        if("folder".equals(name)) {
-            if(StringUtil.isEmpty(value)) {
+        if ("folder".equals(name)) {
+            if (StringUtil.isEmpty(value)) {
                 throw new IllegalParameterValueException(name);
             }
             _folder = value;
+        } else if ("recursive".equals(name)) {
+            _recursive = Boolean.valueOf(value).booleanValue();
         } else {
             throw new UnsupportedParameterException(name);
         }
     }
 
     public String getFolder() {
-        if(StringUtil.isEmpty(_folder)) {
+        if (StringUtil.isEmpty(_folder)) {
             throw new IllegalStateException();
         }
         return _folder;
     }
-    
+
     public Iterator scan() {
-        if(_source == null) {
+        if (_source == null) {
             _source = new ApplicationSourceDescriptor();
             _source.setRoot(getFolder());
         }
-        if(_source.exists() && _source.getFile().isDirectory()) {
+        if (_source.exists() && _source.getFile().isDirectory()) {
             return new FileToSourceIterator(iterateFiles(_source.getFile()));
         }
         return NullIterator.getInstance();
     }
 
     protected Iterator iterateFiles(File dir) {
-        if(dir.exists()) {
+        if (dir.exists()) {
             File[] files;
-            if(dir.isDirectory()) {
-                files = dir.listFiles();
+            if (dir.isDirectory()) {
+                files = listFiles(dir);
             } else {
                 files = new File[] { dir };
             }
@@ -75,54 +82,82 @@ public class FolderSourceScanner implements SourceScanner {
         }
         return NullIterator.getInstance();
     }
-    
+
+    private File[] listFiles(File dir) {
+        File[] files;
+        if (_recursive) {
+            List list = new ArrayList();
+            listFilesRecursive(list, dir);
+
+            files = (File[]) list.toArray(new File[list.size()]);
+        } else {
+            files = dir.listFiles();
+        }
+        return files;
+    }
+
+    private void listFilesRecursive(List list, File dir) {
+        list.add(dir);
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    listFilesRecursive(list, files[i]);
+                } else {
+                    list.add(files[i]);
+                }
+            }
+        }
+    }
+
     private static class FileArrayIterator implements Iterator {
-        
+
         private File[] _files;
+
         private int _index;
-        
+
         private FileArrayIterator(File[] files) {
-            if(files == null) {
+            if (files == null) {
                 throw new IllegalArgumentException();
             }
             _files = files;
         }
-        
+
         public boolean hasNext() {
             return _index < _files.length;
         }
-        
+
         public Object next() {
-            if(hasNext() == false) {
+            if (hasNext() == false) {
                 throw new NoSuchElementException();
             }
             return _files[_index++];
         }
-        
+
         public void remove() {
             throw new UnsupportedOperationException();
         }
-        
+
     }
-    
+
     private class FileToSourceIterator implements Iterator {
-        
+
         private Iterator _iterator;
-        
+
         private FileToSourceIterator(Iterator iterator) {
-            if(iterator == null) {
+            if (iterator == null) {
                 throw new IllegalArgumentException();
             }
             _iterator = iterator;
         }
-        
+
         public boolean hasNext() {
             return _iterator.hasNext();
         }
 
         private String getSystemID(File file) {
             String sourceRoot = _source.getRoot();
-            if(StringUtil.isEmpty(sourceRoot)) {
+            if (StringUtil.isEmpty(sourceRoot)) {
                 sourceRoot = "/";
             }
             String root = _source.getApplication().getRealPath(sourceRoot);
@@ -130,14 +165,13 @@ public class FolderSourceScanner implements SourceScanner {
             String path = absolutePath.substring(root.length());
             return StringUtil.preparePath(path);
         }
-        
+
         public Object next() {
             Object ret = _iterator.next();
-            if(ret instanceof File) {
-                File file = (File)ret;
+            if (ret instanceof File) {
+                File file = (File) ret;
                 String systemID = getSystemID(file);
-                ApplicationSourceDescriptor source = 
-                    new ApplicationSourceDescriptor();
+                ApplicationSourceDescriptor source = new ApplicationSourceDescriptor();
                 source.setRoot(_source.getRoot());
                 source.setSystemID(systemID);
                 source.setFile(file);

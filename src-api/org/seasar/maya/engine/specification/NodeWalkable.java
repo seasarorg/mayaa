f042c96ed78481b07b175a3b6fc671a0b4312d1b
masataka <masataka@64d2bceb-c1f7-0310-a597-f30c3359d46a>
2005-09-14 06:02:33 +0000

    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/maya@881 64d2bceb-c1f7-0310-a597-f30c3359d46a

---
+++ src-api/org/seasar/maya/engine/specification/NodeWalkable.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2004-2005 the Seasar Foundation and the Others.
+ * 
+ * Licensed under the Seasar Software License, v1.1 (aka "the License"); you may
+ * not use this file except in compliance with the License which accompanies
+ * this distribution, and is available at
+ * 
+ *     http://www.seasar.org/SEASAR-LICENSE.TXT
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.seasar.maya.engine.specification;
+
+import java.util.Iterator;
+
+/**
+ * @author Masataka Kurihara (Gluegent, Inc.)
+ */
+public interface NodeWalkable {
+    
+    /**
+     * 親ノードの設定をセットする。
+     * @param parentNode 親ノード。
+     */
+    void setParentNode(SpecificationNode parentNode);
+
+    /**
+     * 親ノードを取得する。
+     * @return 親ノード。
+     */
+    SpecificationNode getParentNode();
+    
+    /**
+     * 子ノードの設定をセットする。
+     * @param childNode 子ノード。
+     */
+    void addChildNode(SpecificationNode childNode);
+
+    /**
+     * 子ノードのイテレータを取得する。
+     * @return 子ノード（<code>SpecificationNode</code>）を保持したイテレータ。
+     */
+    Iterator iterateChildNode();
+    
+}

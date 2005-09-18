5248928cfed2f9021c806c49152712b4e6737a5c
masataka <masataka@64d2bceb-c1f7-0310-a597-f30c3359d46a>
2005-09-18 07:48:54 +0000

    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/maya@959 64d2bceb-c1f7-0310-a597-f30c3359d46a

--- src-impl/org/seasar/maya/impl/util/xml/XmlReaderPool.java
+++ src-impl/org/seasar/maya/impl/util/xml/XmlReaderPool.java
@@ -17,10 +17,14 @@ package org.seasar.maya.impl.util.xml;
 
 import org.apache.xerces.parsers.SAXParser;
 import org.seasar.maya.impl.util.collection.AbstractSoftReferencePool;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.ext.LexicalHandler;
 
 /**
  * @author Masataka Kurihara (Gluegent, Inc.)
@@ -38,7 +42,7 @@ public class XmlReaderPool extends AbstractSoftReferencePool {
         return _xmlReaderPool;
     }
     
-    private XmlReaderPool() {
+    protected XmlReaderPool() {
     }
     
     protected Object createObject() {
@@ -50,22 +54,38 @@ public class XmlReaderPool extends AbstractSoftReferencePool {
         return object instanceof XMLReader;
     }
 
-	public XMLReader borrowXMLReader(DefaultHandler handler, 
+	public XMLReader borrowXMLReader(ContentHandler handler, 
 	        boolean namespaces, boolean validation, boolean xmlSchema) {
 	    XMLReader xmlReader = (XMLReader)borrowObject();
         try {
-            xmlReader.setFeature("http://xml.org/sax/features/namespaces", namespaces);
-            xmlReader.setFeature("http://xml.org/sax/features/validation", validation);
-            xmlReader.setFeature("http://apache.org/xml/features/validation/schema", xmlSchema);
+            xmlReader.setFeature(
+            		"http://xml.org/sax/features/namespaces",
+            		namespaces);
+            xmlReader.setFeature(
+            		"http://xml.org/sax/features/validation",
+            		validation);
+            xmlReader.setFeature(
+            		"http://apache.org/xml/features/validation/schema",
+            		xmlSchema);
+            xmlReader.setContentHandler(handler);
+            if(handler instanceof EntityResolver) {
+                xmlReader.setEntityResolver((EntityResolver)handler);
+            }
+            if(handler instanceof ErrorHandler) {
+                xmlReader.setErrorHandler((ErrorHandler)handler);
+            }
+            if(handler instanceof DTDHandler) {
+                xmlReader.setDTDHandler((DTDHandler)handler);
+            }
+            if(handler instanceof LexicalHandler) {
+                xmlReader.setProperty(
+                		"http://xml.org/sax/properties/lexical-handler", handler);
+            }
         } catch (SAXNotRecognizedException e) {
             throw new RuntimeException(e);
         } catch (SAXNotSupportedException e) {
             throw new RuntimeException(e);
         }
-        xmlReader.setContentHandler(handler);
-        xmlReader.setDTDHandler(handler);
-        xmlReader.setEntityResolver(handler);
-        xmlReader.setErrorHandler(handler);
 	    return xmlReader;
 	}
 	

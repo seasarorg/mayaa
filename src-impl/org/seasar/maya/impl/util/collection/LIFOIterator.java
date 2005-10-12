4016dca9c8c151ba4ba48198821c992cf33ee92a
masataka <masataka@64d2bceb-c1f7-0310-a597-f30c3359d46a>
2005-10-12 17:27:29 +0000

    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/maya@1501 64d2bceb-c1f7-0310-a597-f30c3359d46a

---
+++ src-impl/org/seasar/maya/impl/util/collection/LIFOIterator.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.maya.impl.util.collection;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * @author Masataka Kurihara (Gluegent, Inc)
+ */
+public class LIFOIterator implements Iterator {
+
+	private Stack _stack = new Stack();
+	
+	public void add(Object item) {
+		if(item == null) {
+			throw new IllegalArgumentException();
+		}
+		_stack.push(item);
+	}
+	
+	public void addAll(Iterator it) {
+		while(it.hasNext()) {
+			_stack.push(it.next());
+		}
+	}
+	
+	public void addAll(Enumeration enumeration) {
+		while(enumeration.hasMoreElements()) {
+			_stack.push(enumeration.nextElement());
+		}
+	}
+
+	public boolean hasNext() {
+		return _stack.size() > 0;
+	}
+
+	public Object next() {
+		return _stack.pop();
+	}
+
+	public void remove() {
+	}
+	
+}

9006f01cfd466b308e11858e44191189a63a00ed
SUGA Koji <ko.suga@gmail.com>
2006-01-26 06:25:43 +0000

    fix for reserved namespace "xml:"
    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/mayaa@2295 64d2bceb-c1f7-0310-a597-f30c3359d46a

--- src-impl/org/seasar/mayaa/impl/builder/BuilderUtil.java
+++ src-impl/org/seasar/mayaa/impl/builder/BuilderUtil.java
@@ -67,7 +67,7 @@ public class BuilderUtil implements CONST_IMPL {
                 String attrURI = attr.getQName().getNamespaceURI();
                 if (uri.equals(attrURI)
                         || (mayaa && URI_MAYA.equals(attrURI))
-                        || URI_XML.equals(attrURI)) {
+                        || uri.equals(URI_XHTML) && URI_XML.equals(attrURI)) {
                     String attrValue = attr.getValue();
                     if (needAdjust
                             && adjuster.isTargetAttribute(

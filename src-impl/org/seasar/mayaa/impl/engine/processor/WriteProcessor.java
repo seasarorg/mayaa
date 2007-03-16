2b025779f5d09656730197cf005821d616355002
かとちん <t.katochin@gmail.com>
2007-03-16 15:21:36 +0000

    writeプロセッサを拡張。 ボディを使用できるようにしました。 valueの式内で bodyText変数を参照すると、ボディ内容を取得できるようにしました。
    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/mayaa@2940 64d2bceb-c1f7-0310-a597-f30c3359d46a

--- src-impl/org/seasar/mayaa/impl/engine/processor/WriteProcessor.java
+++ src-impl/org/seasar/mayaa/impl/engine/processor/WriteProcessor.java
@@ -15,21 +15,30 @@
  */
 package org.seasar.mayaa.impl.engine.processor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.seasar.mayaa.cycle.CycleWriter;
 import org.seasar.mayaa.cycle.ServiceCycle;
 import org.seasar.mayaa.engine.Page;
 import org.seasar.mayaa.engine.processor.ProcessStatus;
 import org.seasar.mayaa.engine.processor.ProcessorProperty;
 import org.seasar.mayaa.engine.specification.QName;
 import org.seasar.mayaa.impl.cycle.CycleUtil;
+import org.seasar.mayaa.impl.engine.processor.AbstractAttributableProcessor.ProcesstimeInfo;
+import org.seasar.mayaa.impl.engine.specification.SpecificationUtil;
 import org.seasar.mayaa.impl.util.ObjectUtil;
 import org.seasar.mayaa.impl.util.StringUtil;
 
 /**
  * @author Masataka Kurihara (Gluegent, Inc.)
  */
-public class WriteProcessor extends TemplateProcessorSupport {
+public class WriteProcessor extends AbstractAttributableProcessor {
 
     private static final long serialVersionUID = 9014904982423846249L;
+    private static final String BODY_VARIABLE_NAME = "bodyText";
     private boolean _forHTML;
     private boolean _forHyperText;
     private ProcessorProperty _value;
@@ -42,6 +51,30 @@ public class WriteProcessor extends TemplateProcessorSupport {
         QName originalQName = getOriginalNode().getQName();
         _forHTML = isHTML(originalQName);
         _forHyperText = _forHTML || isXHTML(originalQName);
+        boolean needBodyText;
+        if (_value == null) {
+        	needBodyText = (_injectedNode.getChildNodeSize() > 0);
+        } else {
+        	if (_value.getValue().isLiteral()) {
+        		needBodyText = false;
+        	} else {
+        		// bodyTextを含むスクリプトか？
+        		needBodyText = isExistsBodyTextInScript(_value.getValue().getScriptText());
+        	}
+        }
+        setChildEvaluation(needBodyText);
+    }
+    
+    protected boolean isExistsBodyTextInScript(String scriptText) {
+		String pattern = ".*("
+			+ "\\$\\{"+BODY_VARIABLE_NAME+"\\}|"
+			+ "\\$\\{"+BODY_VARIABLE_NAME+"[^a-zA-Z_$].*\\}|"
+			+ "\\$\\{.*[^a-zA-Z_$]"+BODY_VARIABLE_NAME+"\\}|"
+			+ "\\$\\{.*[^a-zA-Z_$]"+BODY_VARIABLE_NAME+"[^a-zA-Z_$].*\\}"
+			+ ")+.*";
+        Pattern p = Pattern.compile(pattern, Pattern.DOTALL | Pattern.MULTILINE);
+        Matcher m = p.matcher(scriptText);
+		return m.matches();
     }
 
     // MLD property, expectedClass=java.lang.String
@@ -54,49 +87,114 @@ public class WriteProcessor extends TemplateProcessorSupport {
     }
 
     public void setEscapeXml(ProcessorProperty escapeXml) {
+    	checkBoolableProperty(escapeXml);
         _escapeXml = escapeXml;
     }
 
     public void setEscapeWhitespace(ProcessorProperty escapeWhitespace) {
+    	checkBoolableProperty(escapeWhitespace);
         _escapeWhitespace = escapeWhitespace;
     }
 
     public void setEscapeEol(ProcessorProperty escapeEol) {
+    	checkBoolableProperty(escapeEol);
         _escapeEol = escapeEol;
     }
 
-    public ProcessStatus doStartProcess(Page topLevelPage) {
-        if (_value != null) {
-            Object result = _value.getValue().execute(null);
-            String ret = null;
-            boolean empty = StringUtil.isEmpty(result);
-            if (empty && _default != null) {
-                result = _default.getValue().execute(null);
-                if (result != null) {
-                    ret = result.toString();
-                }
-            } else if (empty == false) {
-                ret = String.valueOf(result);
-                if (toBoolean(_escapeXml)) {
-                    ret = StringUtil.escapeXml(ret);
-                }
-                if (_forHyperText && toBoolean(_escapeEol)) {
-                    ret = StringUtil.escapeEol(ret, _forHTML);
-                }
-                if (toBoolean(_escapeWhitespace)) {
-                    ret = StringUtil.escapeWhitespace(ret);
-                }
-            }
-            if (ret != null) {
-                ServiceCycle cycle = CycleUtil.getServiceCycle();
-                cycle.getResponse().write(ret);
-            }
+    private void checkBoolableProperty(ProcessorProperty property) {
+    	if (property == null || property.getValue() == null) {
+    		throw new IllegalArgumentException("needs expression.");
+    	}
+    	if (property.getValue().isLiteral()) {
+    		if (!ObjectUtil.canBooleanConvert(property.getValue().getScriptText())) {
+    			throw new IllegalArgumentException("needs expression.");
+    		}
         }
-        return ProcessStatus.SKIP_BODY;
     }
-
+    
     private boolean toBoolean(ProcessorProperty property) {
         return property != null && ObjectUtil.booleanValue(property.getValue().execute(null), false);
     }
 
+    private void writeValue(String literal) {
+    	Object result;
+    	if (literal != null) {
+    		result = literal;
+    	} else {
+    		if (_value == null) {
+    			result = "";
+    		} else {
+    			result = _value.getValue().execute(null);
+    		}
+    	}
+        String ret = null;
+        boolean empty = StringUtil.isEmpty(result);
+        if (empty && _default != null) {
+            result = _default.getValue().execute(null);
+            if (result != null) {
+                ret = result.toString();
+            }
+        } else if (empty == false) {
+            ret = String.valueOf(result);
+            if (toBoolean(_escapeXml)) {
+                ret = StringUtil.escapeXml(ret);
+            }
+            if (_forHyperText && toBoolean(_escapeEol)) {
+                ret = StringUtil.escapeEol(ret, _forHTML);
+            }
+            if (toBoolean(_escapeWhitespace)) {
+                ret = StringUtil.escapeWhitespace(ret);
+            }
+        }
+        if (ret != null) {
+            write(ret);
+        }
+    }
+    
+	protected ProcessStatus writeStartElement() {
+		if (isChildEvaluation()) {
+			ProcesstimeInfo info = peekProcesstimeInfo();
+			CycleWriter body = info.getBody();
+			String bodyText = body.getString();
+			body.clearBuffer();
+	    	if (_value != null) {
+		        Map variables = new HashMap();
+		        variables.put(BODY_VARIABLE_NAME, bodyText);
+		        SpecificationUtil.startScope(variables);
+		        try {
+		        	writeValue(null);
+		        } finally {
+		        	SpecificationUtil.endScope();
+		        }
+	    	} else {
+	    		if (StringUtil.hasValue(bodyText)) {
+	    			writeValue(bodyText);
+	    		} else {
+	    			writeValue("");
+	    		}
+	    	}
+			return null;
+		}
+		return ProcessStatus.SKIP_BODY;
+	}
+    
+	protected void writeEndElement() {
+		if (isChildEvaluation() == false) {
+			writeValue(null);
+		}
+	}
+	
+	protected void writeBody(String body) {
+		// no-op 
+	}
+	
+	/**
+	 * サイクルに出力する
+	 * @param value
+	 */
+    protected void write(String value) {
+        ServiceCycle cycle = CycleUtil.getServiceCycle();
+        cycle.getResponse().write(value);
+    }
+	
 }

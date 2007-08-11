da76d6a6457fb044a60fbe7e675df5ce4c3edc8b
SUGA Koji <ko.suga@gmail.com>
2007-08-11 15:17:09 +0000

    JIRA[MAYAA-33] request情報が正しくないときにページ名が不正になる問題に対応
    git-svn-id: https://www.seasar.org/svn/mayaa/trunk/mayaa@3163 64d2bceb-c1f7-0310-a597-f30c3359d46a

--- src-impl/org/seasar/mayaa/impl/util/StringUtil.java
+++ src-impl/org/seasar/mayaa/impl/util/StringUtil.java
@@ -220,6 +220,9 @@ public final class StringUtil {
             ret[0] = folder + file;
             ret[1] = "";
         }
+        if (ret[0].length() > 0 && ret[0].charAt(0) != '/') {
+            ret[0] = '/' + ret[0];
+        }
         return ret;
     }
 

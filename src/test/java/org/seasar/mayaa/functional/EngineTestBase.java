2e1e0b00ec12de51403394c66ef6902be3e34f01
mitsu watanabe <mitonize@gmail.com>
2020-05-26 21:57:36 +0900

    WIP:Engineに対する動作テスト整備の基礎(要Java7以上)

---
+++ src/test/java/org/seasar/mayaa/functional/EngineTestBase.java
@@ -0,0 +1,176 @@
+package org.seasar.mayaa.functional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.logging.LogManager;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.seasar.mayaa.FactoryFactory;
+import org.seasar.mayaa.engine.Engine;
+import org.seasar.mayaa.impl.FactoryFactoryImpl;
+import org.seasar.mayaa.impl.cycle.CycleUtil;
+import org.seasar.mayaa.impl.provider.ProviderUtil;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+
+public class EngineTestBase {
+
+    private MockServletContext servletContext;
+    private Engine engine;
+
+    @BeforeClass
+    public static void init() throws SecurityException, IOException {
+        try (InputStream in = EngineTestBase.class.getClassLoader().getResourceAsStream("jul.properties")) {
+            LogManager.getLogManager().readConfiguration(in);
+        }
+
+        FactoryFactory.release();
+        FactoryFactory.setInstance(new FactoryFactoryImpl());
+    }
+
+    @Before
+    public void setup() throws SecurityException, IOException {
+        final Object _testClassInstance = this;
+        servletContext = new MockServletContext() {
+            /**
+             * {@link ServletContext#getResource(String)}の代替実装。
+             * テストクラスのパッケージからの相対パスとして取得する。
+             * 
+             * @param path 要求されているリソースのパス
+             */
+            public URL getResource(String path) throws MalformedURLException {
+                if (path.startsWith("/")) {
+                    path = path.substring(1);
+                }
+                return _testClassInstance.getClass().getResource(path);
+            }
+        };
+
+        FactoryFactory.setContext(servletContext);
+
+        engine = ProviderUtil.getEngine();
+        // engine.setParameter(EngineImpl.DUMP_ENABLED, "true");
+        // engine.setParameter(EngineImpl.PAGE_SERIALIZE, "true");
+    }
+
+    /**
+     * テスト実行に使用するリクエストオブジェクトを作成する
+     * 
+     * @param path 処理するHTMLファイルへのパス（クラスパスルートからのパス）
+     * @return リクエストオブジェクト（モック）
+     */
+    MockHttpServletRequest createRequest(final String path) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setServletPath(path);
+        return request;
+    }
+
+    /**
+     * テストの実行に設定されているServletContextを取得する
+     * 
+     * @return ServletContextオブジェクト
+     */
+    MockServletContext getServletContext() {
+        return servletContext;
+    }
+
+    /**
+     * 指定したHTMLファイルに対してEngineの本体処理を呼び出す
+     * 
+     * @param request            モック化されたリクエストオブジェクト
+     * @param pageScopeAttribute あらかじめページスコープに定義済のものとして引き渡す属性のマップ
+     * @return レスポンスオブジェクト（モック）
+     */
+    MockHttpServletResponse exec(final MockHttpServletRequest request, final Map<String, Object> pageScopeAttribute) {
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+
+        CycleUtil.initialize(request, response);
+        engine.doService(pageScopeAttribute, true);
+
+        // verify(servletContext).getRealPath(path);
+
+        return response;
+    }
+
+    /**
+     * Engineの処理結果として期待する内容に合致しているかを検証する。
+     * 
+     * @param expectedContentPath 機体結果の内容が保管されているファイルへのパス（クラスパスルートからのパス）
+     * @throws IOException IOエラーが発生した場合
+     */
+    void verifyResponse(final MockHttpServletResponse response, final String expectedContentPath) throws IOException {
+
+        final URL url = getClass().getResource(expectedContentPath);
+        if (url == null) {
+            fail("Specified file is not found. " + expectedContentPath);
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    line = reader.readLine();
+                    break;
+                }
+                if (!line.matches("\\w.*")) {
+                    break;
+                }
+                final String headerPair[] = line.split(":", 2);
+                if (headerPair.length == 2) {
+                    final String value = response.getHeader(headerPair[0]);
+                    assertEquals("Response header is not match. " + headerPair[0], headerPair[1], value);
+                }
+            }
+
+            // Process Body
+            final StringBuilder expectedBody = new StringBuilder();
+            while (line != null) {
+                expectedBody.append(line);
+                line = reader.readLine();
+                if (line != null) {
+                    expectedBody.append('\n');
+                }
+            }
+
+            final String content = response.getContentAsString();
+
+            final String expected = expectedBody.toString().replace(' ', '.').replace('\n', '/');
+            final String actual = content.replace(' ', '.').replace('\n', '/');
+            assertEquals(expected, actual);
+        }
+
+        // for (String headerName: response.getHeaderNames()) {
+        // System.out.println(headerName + ":" + response.getHeader(headerName));
+        // }
+    }
+
+    /**
+     * 第一引数に指定したファイルパスを処理した結果を第二引数の内容と比較する。 
+     * 指定するパスはクラスパスルート( src/test/resources)をルートとする。
+     * 
+     * @param targetContentPath
+     * @param expectedContentPath
+     * @param pageScopeAttribute
+     */
+    void execAndVerify(final String targetContentPath, final String expectedContentPath,
+            final Map<String, Object> pageScopeAttribute) throws IOException {
+        final MockHttpServletRequest request = createRequest(targetContentPath);
+
+        // When
+        final MockHttpServletResponse response = exec(request, pageScopeAttribute);
+
+        // Then
+        verifyResponse(response, expectedContentPath);
+    }
+
+}

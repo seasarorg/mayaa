127c5c1cf62b1aedc53a5d1a8dde4f8b3d8fe325
MW <mttk.wtnb@gmail.com>
2020-06-03 22:12:39 +0900

    #15 ScriptEnvironment内のキャッシュサイズをファクトリ定義XMLで指定する (#17)
    #15 ScriptEnvironment内のキャッシュサイズをファクトリ定義XMLで指定する

--- src-impl/org/seasar/mayaa/impl/cycle/script/rhino/ScriptEnvironmentImpl.java
+++ src-impl/org/seasar/mayaa/impl/cycle/script/rhino/ScriptEnvironmentImpl.java
@@ -49,12 +49,16 @@ import org.seasar.mayaa.source.SourceDescriptor;
 public class ScriptEnvironmentImpl extends AbstractScriptEnvironment {
     private static final long serialVersionUID = -4067264733660357274L;
     private static Scriptable _standardObjects;
-    private static final WeakValueHashMap/*<String, CompiledScript>*/ scriptCache = new WeakValueHashMap(128);
+    private static WeakValueHashMap/*<String, CompiledScript>*/ scriptCache;
 
     private static final boolean CONSTRAINT_GLOBAL_PROPERTY_DEFINE = true;
 
     // singleton
     private static WrapFactory _wrap;
+    private static int _cacheSize = 128;
+    static {
+        setScriptCacheSize(_cacheSize);
+    }
 
     private boolean _useGetterScriptEmulation;
 
@@ -248,6 +252,15 @@ public class ScriptEnvironmentImpl extends AbstractScriptEnvironment {
         return (scriptObject instanceof Scriptable);
     }
 
+    static void setScriptCacheSize(int cacheSize) {
+        _cacheSize = cacheSize;
+        scriptCache = new WeakValueHashMap(cacheSize);
+    }
+
+    static int getScriptCacheSize() {
+        return _cacheSize;
+    }
+
     static void setWrapFactory(WrapFactory wrap) {
         _wrap = wrap;
     }
@@ -275,6 +288,11 @@ public class ScriptEnvironmentImpl extends AbstractScriptEnvironment {
                 throw new IllegalParameterValueException(getClass(), name);
             }
             _useGetterScriptEmulation = ObjectUtil.booleanValue(value, false);
+        } else if ("cacheSize".equals(name)) {
+            if (StringUtil.isEmpty(value)) {
+                throw new IllegalParameterValueException(getClass(), name);
+            }
+            setScriptCacheSize(Integer.parseInt(value));
         }
         super.setParameter(name, value);
     }

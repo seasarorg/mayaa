09de0e859e1a75386753f536ea991e1f6019a8ca
Mitsutaka Watanabe <mttk.wtnb@gmail.com>
2020-06-06 01:49:28 +0900

    コミット漏れ（複数のEngineを使ったテストクラスを実行できるようにする。）

--- src/test/java/org/seasar/mayaa/impl/cycle/script/rhino/ScriptEnvironmentTest.java
+++ src/test/java/org/seasar/mayaa/impl/cycle/script/rhino/ScriptEnvironmentTest.java
@@ -1,24 +1,32 @@
 package org.seasar.mayaa.impl.cycle.script.rhino;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.seasar.mayaa.FactoryFactory;
 import org.seasar.mayaa.provider.ProviderFactory;
 
 import org.seasar.mayaa.impl.FactoryFactoryImpl;
 import org.seasar.mayaa.impl.cycle.scope.StubApplicationScopeSupport;
 
-import junit.framework.TestCase;
+public class ScriptEnvironmentTest {
 
-public class ScriptEnvironmentTest extends TestCase {
+    @BeforeClass
+    public static void init() {
+        FactoryFactory.release();
+    }
 
+    @Test
     public void testキャッシュサイズの指定() {
         //-- Given
-        // ./WEB-INF/org.seasar.mayaa.provider.ServiceProvider 内の
-        // scriptEnvironment cacheSize に 256 が設定されている
+        // * ./WEB-INF/org.seasar.mayaa.provider.ServiceProvider 内の
+        //   scriptEnvironment cacheSize に 256 が設定されている
+        FactoryFactory.setInstance(new FactoryFactoryImpl());
+        FactoryFactory.setContext(new StubApplicationScopeSupport(this));
 
         //-- When
         // ファクトリーを経由してScriptEnvironmentインスタンスを生成する
-        FactoryFactory.setInstance(new FactoryFactoryImpl());
-        FactoryFactory.setContext(new StubApplicationScopeSupport(this));
         ProviderFactory providerFactory = (ProviderFactory) FactoryFactory.getFactory(ProviderFactory.class);
         // ServiceProviderのインスタンスを取得することでScriptEnvironmentを作成する。
         providerFactory.getServiceProvider();

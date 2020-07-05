d19383faf4cc414995b54f4634a9f6ef8cc027b1
Mitsutaka Watanabe <mttk.wtnb@gmail.com>
2020-07-05 13:52:22 +0900

    プロセッサのテストを追加（ for, forEach ）

---
+++ src/test/java/org/seasar/mayaa/functional/ProcessorTest.java
@@ -0,0 +1,36 @@
+package org.seasar.mayaa.functional;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.seasar.mayaa.impl.engine.processor.TooManyLoopException;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+public class ProcessorTest extends EngineTestBase {
+    static final String BASE_PATH = "/it-case/html-transform/processors/";
+
+    @Test
+    public void プロセッサforで要素を繰り返し出力する() throws IOException {
+        execAndVerify(BASE_PATH + "for/target.html", BASE_PATH + "for/expected.html", null);
+    }
+
+    @Test
+    public void プロセッサforでMaxに指定した回数を超えるとTooManyLoopException() throws IOException {
+        try {
+            // When
+            final MockHttpServletRequest request = createRequest(BASE_PATH + "for/target_exception.html");
+            exec(request, null);
+            fail("TooManyLoopExceptionが発生するはず");
+        } catch (TooManyLoopException e) {
+            ;
+        }
+    }
+
+    @Test
+    public void プロセッサforeachで要素を繰り返し出力する() throws IOException {
+        execAndVerify(BASE_PATH + "foreach/target.html", BASE_PATH + "foreach/expected.html", null);
+    }
+
+}

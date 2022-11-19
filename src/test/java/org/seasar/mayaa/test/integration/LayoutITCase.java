09d6f04e86e869e0d852a1c3c055f7ed033659dc
MW <mttk.wtnb@gmail.com>
2022-11-19 11:42:20 +0900

    ブラウザテストはMaven内での実行の時だけ実行する

--- src/test/java/org/seasar/mayaa/test/integration/LayoutITCase.java
+++ src/test/java/org/seasar/mayaa/test/integration/LayoutITCase.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -57,6 +58,7 @@ public class LayoutITCase extends WebDriverBase {
 
     @ParameterizedTest(name = "{0}: path={1}")
     @MethodSource
+    @EnabledIfSystemProperty(named = "inMaven", matches = "1")
     public void test(String title, String path, Command[] commands) {
         runTest(title, path, commands);
     }

bb30a3809fa010c13ac23b8515f41abddeb047f4
MW <mttk.wtnb@gmail.com>
2023-01-29 17:08:35 +0900

    EngineTestも両パーサでテスト実施

--- src/test/java/org/seasar/mayaa/functional/engine/EngineTest.java
+++ src/test/java/org/seasar/mayaa/functional/engine/EngineTest.java
@@ -23,9 +23,12 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.seasar.mayaa.engine.error.ErrorHandler;
 import org.seasar.mayaa.functional.EngineTestBase;
 import org.seasar.mayaa.impl.CONST_IMPL;
+import org.seasar.mayaa.impl.builder.TemplateBuilderImpl;
 import org.seasar.mayaa.impl.builder.library.NoRequiredPropertyException;
 import org.seasar.mayaa.impl.engine.error.TemplateErrorHandler;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -33,15 +36,22 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 public class EngineTest extends EngineTestBase {
 
-    @Test
-    public void beforeRender() throws IOException {
+    void setUseNewParser(boolean useNewParser) {
+        getServiceProvider().getTemplateBuilder().setParameter(TemplateBuilderImpl.USE_NEW_PARSER, Boolean.toString(useNewParser));
+    }
+
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void beforeRender(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/beforeRender/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void 単純ケース() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void 単純ケース(boolean useNewParser)throws IOException {
         // Given
         getServletContext().setAttribute("s0", "sv0");
 
@@ -55,46 +65,58 @@ public class EngineTest extends EngineTestBase {
         verifyResponse(response, "/it-case/engine/simple/expected.html");
     }
 
-    @Test
-    public void HTMLで指定したCharsetがレスポンスヘッダに設定される() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void HTMLで指定したCharsetがレスポンスヘッダに設定される(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/html-transform/charset/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
     @Disabled
-    public void HTML5形式で指定したCharsetがレスポンスヘッダに設定される() throws IOException {
+    public void HTML5形式で指定したCharsetがレスポンスヘッダに設定される(boolean useNewParser)throws IOException {
         // Mayaaは <meta charset="XX"> の形式の記述には対応していないので ＠Ignore する。
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/html-transform/charset-html5/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
 
-    @Test
-    public void スクリプト実行_Javaクラス参照() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void スクリプト実行_Javaクラス参照(boolean useNewParser)throws IOException {
         System.setProperty("test.value", "TEST VALUE");
 
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/script/java-bridge/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void cdata() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void cdata(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/cdata/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void charset_html() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void charset_html(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/charset_html/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void charset_xhtml() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void charset_xhtml(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/charset_xhtml/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
@@ -104,9 +126,11 @@ public class EngineTest extends EngineTestBase {
      * java.lang.Throwable.mayaa にてエラーページにフォワードさせる。
      * @throws IOException
      */
-    @Test
-    public void error_forward() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void error_forward(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/error_forward/";
 
         ErrorHandler errorHandler = new TemplateErrorHandler();
@@ -123,9 +147,11 @@ public class EngineTest extends EngineTestBase {
      * java.lang.Throwable.mayaa にてエラーページにリダイレクトさせる。
      * @throws IOException
      */
-    @Test
-    public void error_redirect() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void error_redirect(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/error_redirect/";
 
         ErrorHandler errorHandler = new TemplateErrorHandler();
@@ -142,88 +168,114 @@ public class EngineTest extends EngineTestBase {
         assertEquals("/it-case/engine/error_redirect/redirected.html?message=error_redirected&title=tests_1_18", response.getHeader("Location"));
     }
     
-    @Test
-    public void escape_html() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void escape_html(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/escape/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void escape_xhtml() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void escape_xhtml(boolean useNewParser)throws IOException {
         // getServiceProvider().getTemplateBuilder();
         setEngineParameter(CONST_IMPL.TEMPLATE_PATH_PATTERN, ".*\\.(html|xml|xhtml)");
 
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/escape_xhtml/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void escape_xml() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void escape_xml(boolean useNewParser)throws IOException {
         setEngineParameter(CONST_IMPL.TEMPLATE_PATH_PATTERN, ".*\\.(html|xml|xhtml)");
 
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/escape/";
         execAndVerify(DIR + "target.xml", DIR + "expected.xml", null);
     }
 
-    @Test
-    public void forward() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void forward(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/forward/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void ignore_mayaa() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void ignore_mayaa(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/ignore_mayaa/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_include() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_include(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_include/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_mayaa() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_mayaa(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_mayaa/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_no() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_no(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_no/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_target_is_empty() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_target_is_empty(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_target_is_empty/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_template() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_template(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_template/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void inject_xpath() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void inject_xpath(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/inject_xpath/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void no_xmlns() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void no_xmlns(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/no_xmlns/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void redirect() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void redirect(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/redirect/";
         MockHttpServletRequest request = createRequest(DIR + "target.html");
 
@@ -234,14 +286,18 @@ public class EngineTest extends EngineTestBase {
         assertEquals("/it-case/engine/redirect/redirected.html?message=redirected&title=tests_1_16", response.getHeader("Location"));
     }
 
-    @Test
-    public void replace() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void replace(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/replace/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void required() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void required(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/required/";
         MockHttpServletRequest request = createRequest(DIR + "target.html");
 
@@ -255,8 +311,10 @@ public class EngineTest extends EngineTestBase {
         }
     }
 
-    @Test
-    public void required_not_empty() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void required_not_empty(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/required_not_empty/";
         MockHttpServletRequest request = createRequest(DIR + "target.html");
 
@@ -270,28 +328,37 @@ public class EngineTest extends EngineTestBase {
         }
     }
 
-    @Test
-    public void script() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void script(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/script/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void template_attribute() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void template_attribute(boolean useNewParser)throws IOException {
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/template_attribute/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void undefined_identifier() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void undefined_identifier(boolean useNewParser)throws IOException {
         enableDump();
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/undefined_identifier/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
-    @Test
-    public void xout() throws IOException {
+    @ParameterizedTest(name = "useNewParser {0}")
+    @ValueSource(booleans = {false, true})
+    public void xout(boolean useNewParser) throws IOException {
+        setUseNewParser(useNewParser);
+        setUseNewParser(useNewParser);
         final String DIR = "/it-case/engine/xout/";
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }

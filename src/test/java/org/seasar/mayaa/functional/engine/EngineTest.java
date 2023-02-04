b7936cda31193d359ce171762c7c47aaabfa8dd5
MW <mttk.wtnb@gmail.com>
2023-02-04 22:17:37 +0900

    Issue #13 独自プロセッサーを使うとエラーが発生する問題の再現コード

--- src/test/java/org/seasar/mayaa/functional/engine/EngineTest.java
+++ src/test/java/org/seasar/mayaa/functional/engine/EngineTest.java
@@ -21,8 +21,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.seasar.mayaa.engine.error.ErrorHandler;

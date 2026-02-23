a98c9f21098651453b6b022cd1507b3580d0fbd9
MW <mttk.wtnb@gmail.com>
2026-02-23 13:50:37 +0900

    fix: Revert to simple Spring Boot app to let web.xml be auto-discovered

--- test-war/src/spring-boot/java/org/seasar/mayaa/test/boot/TestWarApplication.java
+++ test-war/src/spring-boot/java/org/seasar/mayaa/test/boot/TestWarApplication.java
@@ -1,24 +1,12 @@
 package org.seasar.mayaa.test.boot;
 
-import org.seasar.mayaa.impl.MayaaServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestWarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestWarApplication.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean<?> mayaaServlet() {
-        MayaaServlet servlet = new MayaaServlet();
-        ServletRegistrationBean<?> bean = new ServletRegistrationBean<>(servlet, "*.html");
-        bean.setName("MayaaServlet");
-        bean.setLoadOnStartup(1);
-        return bean;
     }
 }

464d1e28b25ca7c6fbab9a586177b3b731d4c83e
MW <mitonize@gmail.com>
2026-05-09 13:42:47 +0900

    test(customtag): __parent__ 廃止に対応し simpletest を観点別テストに分解 (#134)
    Rhino 1.9.1 では __parent__ プロパティが廃止（1.7R3〜R4 で削除）のため、
    simpletest の hello2/hello3 は RuntimeException + DiagnosticEventBuffer ERROR を
    期待するテストに変更する。
    
    - CustomTagTest: simpletest() を hello1〜hello5 の個別テストメソッドに分解
      - hello1: SimpleTag 基本レンダリング
      - hello2: page.__parent__ 廃止 → 例外 + DiagnosticEventBuffer ERROR 検証
      - hello3: this.__parent__.__parent__ 廃止 → 例外 + DiagnosticEventBuffer ERROR 検証
      - hello4: _['class'] WalkStandardScope によるクラス属性取得
      - hello5: SimpleTag が JSTL c:if 内で IfTag を親として検出できる
    - 各テストは DynamicRegisteredSourceHolder でインライン定義し、
      ページキャッシュ競合を避けるため固有パス (simpletest_h1〜h5) を使用
    - simpletest/target.mayaa: __parent__ 参照をコメントアウト、
      代替テキストを配置（非互換であることを明示）
    - simpletest/expected.html: 上記に合わせて期待値を更新

--- src/test/java/org/seasar/mayaa/functional/customtag/CustomTagTest.java
+++ src/test/java/org/seasar/mayaa/functional/customtag/CustomTagTest.java
@@ -16,12 +16,18 @@
 package org.seasar.mayaa.functional.customtag;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.seasar.mayaa.functional.EngineTestBase;
+import org.seasar.mayaa.impl.management.DiagnosticEventBuffer;
+import org.seasar.mayaa.impl.source.DynamicRegisteredSourceHolder;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 
 public class CustomTagTest extends EngineTestBase {
@@ -91,10 +97,186 @@ public class CustomTagTest extends EngineTestBase {
         execAndVerify(DIR + "target.html", DIR + "expected.html", null);
     }
 
+    @AfterEach
+    public void cleanup() {
+        DynamicRegisteredSourceHolder.unregisterAll();
+    }
+
+    // SimpleTag の基本動作: page スコープから取得した値と simpleName を出力する
     @Test
-    public void simpletest() throws IOException {
-        final String DIR = "/it-case/customtag/simpletest/";
-        execAndVerify(DIR + "target.html", DIR + "expected.html", null);
+    public void simpletest_hello1_customSimpleTag() throws IOException {
+        final String DIR = "/it-case/customtag/simpletest_h1/";
+
+        String targetHtml = """
+                <html><body>
+                <span id="hello1" class="c&lt;l&amp;a&quot;s&gt;s" style="display:inline">dummy</span>
+                </body></html>
+                """;
+        String targetMayaa = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <m:mayaa xmlns:m="http://mayaa.seasar.org"
+                        xmlns:mt="http://mayaa.seasar.org/test/mayaa-test">
+                    <m:beforeRender><![CDATA[
+                        page['SimpleTestTag'] = "hello ";
+                    ]]></m:beforeRender>
+                    <m:echo m:id="hello1">
+                        <mt:simpleTest simpleName="${ 'TestTag' }" />
+                    </m:echo>
+                </m:mayaa>
+                """;
+
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.html", targetHtml);
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.mayaa", targetMayaa);
+
+        MockHttpServletResponse response = exec(createRequest(DIR + "target.html"), null);
+        assertTrue(response.getContentAsString().contains("hello TestTag"),
+                "SimpleTestTag should render 'hello TestTag'");
+    }
+
+    /*
+     * [エンジン別動作履歴]
+     * rhino:js:1.7R2          → 成功 (__parent__ は利用可能)
+     * org.mozilla:rhino:1.9.1 → 失敗 __parent__ は Rhino 1.7R3〜R4 で廃止。
+     *                           レンダリング時に例外が発生し DiagnosticEventBuffer にエラーが記録される。
+     */
+    @Test
+    public void simpletest_hello2_pageParentDeprecated() throws IOException {
+        final String DIR = "/it-case/customtag/simpletest_h2/";
+
+        String targetHtml = """
+                <html><body>
+                <span id="hello2">dummy</span>
+                </body></html>
+                """;
+        String targetMayaa = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <m:mayaa xmlns:m="http://mayaa.seasar.org">
+                    <m:echo m:id="hello2">
+                        [<m:write value="${ page.__parent__['class']; }"/>]
+                    </m:echo>
+                </m:mayaa>
+                """;
+
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.html", targetHtml);
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.mayaa", targetMayaa);
+
+        try {
+            exec(createRequest(DIR + "target.html"), null);
+            fail("Expected RuntimeException due to deprecated __parent__ property");
+        } catch (RuntimeException e) {
+            // expected
+        }
+
+        List<DiagnosticEventBuffer.Event> events = capture.snapshot();
+        assertTrue(
+                events.stream().anyMatch(e -> e.level() == DiagnosticEventBuffer.Level.ERROR),
+                "DiagnosticEventBuffer should record an error from deprecated page.__parent__");
+    }
+
+    /*
+     * [エンジン別動作履歴]
+     * rhino:js:1.7R2          → 成功 (__parent__ は利用可能)
+     * org.mozilla:rhino:1.9.1 → 失敗 __parent__ は Rhino 1.7R3〜R4 で廃止。
+     *                           レンダリング時に例外が発生し DiagnosticEventBuffer にエラーが記録される。
+     */
+    @Test
+    public void simpletest_hello3_thisParentDeprecated() throws IOException {
+        final String DIR = "/it-case/customtag/simpletest_h3/";
+
+        String targetHtml = """
+                <html><body>
+                <span id="hello3">dummy</span>
+                </body></html>
+                """;
+        String targetMayaa = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <m:mayaa xmlns:m="http://mayaa.seasar.org">
+                    <m:echo m:id="hello3">
+                        <m:with>
+                            [<m:write value="${ this.__parent__.__parent__['class']; }"/>]
+                        </m:with>
+                    </m:echo>
+                </m:mayaa>
+                """;
+
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.html", targetHtml);
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.mayaa", targetMayaa);
+
+        try {
+            exec(createRequest(DIR + "target.html"), null);
+            fail("Expected RuntimeException due to deprecated __parent__ property");
+        } catch (RuntimeException e) {
+            // expected
+        }
+
+        List<DiagnosticEventBuffer.Event> events = capture.snapshot();
+        assertTrue(
+                events.stream().anyMatch(e -> e.level() == DiagnosticEventBuffer.Level.ERROR),
+                "DiagnosticEventBuffer should record an error from deprecated this.__parent__");
+    }
+
+    // _ (WalkStandardScope) でカレントノードの class 属性を取得する
+    @Test
+    public void simpletest_hello4_scopeWalk() throws IOException {
+        final String DIR = "/it-case/customtag/simpletest_h4/";
+
+        String targetHtml = """
+                <html><body>
+                <span id="hello4" class="c&lt;l&amp;a&quot;s&gt;s" style="display:inline">dummy</span>
+                </body></html>
+                """;
+        String targetMayaa = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <m:mayaa xmlns:m="http://mayaa.seasar.org">
+                    <m:echo m:id="hello4">
+                        <m:with>
+                            [<m:write value="${ _['class']; }"/>]
+                        </m:with>
+                    </m:echo>
+                </m:mayaa>
+                """;
+
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.html", targetHtml);
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.mayaa", targetMayaa);
+
+        MockHttpServletResponse response = exec(createRequest(DIR + "target.html"), null);
+        assertTrue(response.getContentAsString().contains("[c&lt;l&amp;a\"s&gt;s]"),
+                "Scope walk _['class'] should return the span's class attribute value");
+    }
+
+    // SimpleTag が JSTL c:if の内側に配置されたとき、親タグとして IfTag を検出できる
+    @Test
+    public void simpletest_hello5_simpleTagInsideJstlIf() throws IOException {
+        final String DIR = "/it-case/customtag/simpletest_h5/";
+
+        String targetHtml = """
+                <html><body>
+                <span id="hello5"><span id="hello6">dummy</span></span>
+                </body></html>
+                """;
+        String targetMayaa = """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <m:mayaa xmlns:m="http://mayaa.seasar.org"
+                        xmlns:mt="http://mayaa.seasar.org/test/mayaa-test"
+                        xmlns:c="http://java.sun.com/jstl/core_rt">
+                    <m:beforeRender><![CDATA[
+                        page['SimpleTestTag'] = "hello ";
+                    ]]></m:beforeRender>
+                    <m:echo m:id="hello5">
+                        <c:if test="${ true }"><m:doBody /></c:if>
+                    </m:echo>
+                    <mt:simpleTest m:id="hello6" simpleName="${ 'TestTag' }" />
+                </m:mayaa>
+                """;
+
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.html", targetHtml);
+        DynamicRegisteredSourceHolder.registerContents(DIR + "target.mayaa", targetMayaa);
+
+        MockHttpServletResponse response = exec(createRequest(DIR + "target.html"), null);
+        assertTrue(response.getContentAsString().contains("hello TestTag"),
+                "SimpleTag inside JSTL c:if should render 'hello TestTag'");
+        assertTrue(response.getContentAsString().contains("org.apache.taglibs.standard.tag.rt.core.IfTag"),
+                "SimpleTag should detect IfTag as ancestor via parent chain");
     }
 
     @Test
